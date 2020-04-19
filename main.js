0fd1718005c5f11d6af9a98c2c1259d248b8ba56
Abdullah Faruk ÇİFTLER <37593465+farukciftler@users.noreply.github.com>
2020-04-19 21:17:01 +0300

    menü eklendi

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow(s{});
+    mainWindow = new BrowserWindow({});
+
+    console.log(process.platform);
+
     mainWindow.loadURL(
         url.format( {
             pathname: path.join(__dirname, "main.html"),
@@ -15,4 +18,58 @@ app.on('ready', () => {
             slashes: true
         })
     );
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+    Menu.setApplicationMenu(mainMenu);
 })
+
+
+
+
+const mainMenuTemplate = [
+    {
+        label:"Dosya",
+        submenu: [
+            {
+                label: "Aç"
+            },
+            {
+                label: "Export"
+            },
+            {
+                label: "Çıkış",
+                accelerator: process.platform == "darwin" ? "Command + Q" : "Ctrl+Q",
+                role: "quit" 
+            }
+             
+        ]
+    }
+]
+
+//macintoshta hata verecek menü sisteminin hata vermemesi için yazılan kod
+if(process.platform == "darwin"){
+    mainMenuTemplate.unshift({
+        label: app.getName(),
+        role: "Scenario"
+    })
+}
+if(process.env.NODE_ENV !== "production "){
+    mainMenuTemplate.push(
+        {
+            label:"Dev tools",
+            submenu: [
+                {
+                    label: "Geliştirici Penceresini Aç",
+                    click(item, focusedWindows){
+                        focusedWindows.toggleDevTools();
+                    }
+                },
+                {
+                    label: "yenile",
+                    role: "reload"
+                }
+            ]
+        }
+    )
+}
