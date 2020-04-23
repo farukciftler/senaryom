64707f4a35080541f901d2b080ef301a7730ef17
Ahmet Emin Kayar <github@aeminkyr.com>
2020-04-23 15:46:56 +0300

    ck editor added

--- main.js
+++ main.js
@@ -6,84 +6,72 @@ const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
 
-app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        webPreferences: {
-                nodeIntegration: true
-              }
-        });
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
-    console.log(process.platform);
+  console.log(process.platform);
 
-    mainWindow.loadURL(
-        url.format( {
-            pathname: path.join(__dirname, "main.html"),
-            protocol: "file:",
-            slashes: true
-        })
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "main.html"),
+      protocol: "file:",
+      slashes: true,
+    })
     );
 
-    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
 
-    Menu.setApplicationMenu(mainMenu);
+  Menu.setApplicationMenu(mainMenu);
 
-    ipcMain.on("key",(err,data)=>{
-        console.log(data);
-
-    })
-
-
-
-
-
-})
-
-
-
+  ipcMain.on("key", (err, data) => {
+    console.log(data);
+  });
+});
 
 const mainMenuTemplate = [
-    {
-        label:"Dosya",
-        submenu: [
-            {
-                label: "Aç"
-            },
-            {
-                label: "Export"
-            },
-            {
-                label: "Çıkış",
-                accelerator: process.platform == "darwin" ? "Command + Q" : "Ctrl+Q",
-                role: "quit" 
-            }
-             
-        ]
-    }
-]
+{
+  label: "Dosya",
+  submenu: [
+  {
+    label: "Aç",
+  },
+  {
+    label: "Export",
+  },
+  {
+    label: "Çıkış",
+    accelerator: process.platform == "darwin" ? "Command + Q" : "Ctrl+Q",
+    role: "quit",
+  },
+  ],
+},
+];
 
 //macintoshta hata verecek menü sisteminin hata vermemesi için yazılan kod
-if(process.platform == "darwin"){
-    mainMenuTemplate.unshift({
-        label: app.getName(),
-        role: "Scenario"
-    })
+if (process.platform == "darwin") {
+  mainMenuTemplate.unshift({
+    label: app.getName(),
+    role: "Scenario",
+  });
 }
-if(process.env.NODE_ENV !== "production "){
-    mainMenuTemplate.push(
-        {
-            label:"Dev tools",
-            submenu: [
-                {
-                    label: "Geliştirici Penceresini Aç",
-                    click(item, focusedWindows){
-                        focusedWindows.toggleDevTools();
-                    }
-                },
-                {
-                    label: "yenile",
-                    role: "reload"
-                }
-            ]
-        }
-    )
+if (process.env.NODE_ENV !== "production ") {
+  mainMenuTemplate.push({
+    label: "Dev tools",
+    submenu: [
+    {
+      label: "Geliştirici Penceresini Aç",
+      click(item, focusedWindows) {
+        focusedWindows.toggleDevTools();
+      },
+    },
+    {
+      label: "yenile",
+      role: "reload",
+    },
+    ],
+  });
 }
